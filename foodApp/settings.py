6ce155141343a8dea20e2ed9ca1b9173c2bc6807
aparn-gupta <aparnagupta813@gmail.com>
2026-02-19 18:50:54 +0530

    cors config, data fetching working

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -43,11 +43,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'django.contrib.postgres'
+    'django.contrib.postgres',
+    'corsheaders'
+
+
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +60,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://192.168.1.15:19000",
+#     "http://localhost:3000",
+# ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'foodApp.urls'
 
