091cf326efe5364df8bec5078dacec8428ffe52f
aparn-gupta <aparnagupta813@gmail.com>
2026-02-22 20:47:45 +0530

    load dishes api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import AllFood, FoodDetails, options
+from .views import AllFood, FoodDetails, options, loadDishes
 
 
 
 urlpatterns = [
     path('list/',  AllFood.as_view(), name='all-food-list'),
     path('update/<int:id>/', FoodDetails.as_view(), name='food-details-update'),
-    path('options/', options, name='options'  )
+    path('options/', options, name='options'  ),
+    path('preferences/', loadDishes, name='preferences')
+
 ]
