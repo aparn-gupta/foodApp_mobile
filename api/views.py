245b865f2077050df2eb7953631357877ff82ca8
aparn-gupta <aparnagupta813@gmail.com>
2026-02-22 18:46:00 +0530

    options set rendering, working

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Food
 from .serializers import FoodSerializer
 from django.contrib.postgres.search import SearchQuery
 from django.db.models import Func, F
+from rest_framework.response import Response
 
 
 class AllFood(generics.ListCreateAPIView):
@@ -25,24 +27,48 @@ def options(req):
     # queryset = Food.objects.all()
     requestedOption = req.GET.get("requested")
 
-    if requestedOption != 'main_ingredients' and requestedOption != 'optional_ngredients':
+    ALLOWED_FIELDS = ["name", "taste", "calories", "prep_time", "recipe", "cooking_method", "ingredients", "region" ]
+
+    if requestedOption not in ALLOWED_FIELDS:
+        return JsonResponse({"error": "Invalid requested"}, status=400)
+
+    if requestedOption != 'ingredients':
 
       queryset =   Food.objects.values_list(requestedOption, flat=True).distinct()
 
-    else: 
+    else:
 
-       queryset =  Food.objects.annotate(ingredient=Func(requestedOption), function="unnest").values_list(requestedOption, flat=True).distinct()
+        queryset = (
+            Food.objects
+            .annotate(
+                ingredient=Func(
+                    F("main_ingredients"),
+                    function="unnest"
+                )
+            )
+            .values_list("ingredient", flat=True)
+            .distinct()
+        )
+
+
+    return JsonResponse(list(queryset), safe=False)
 
 
 
-   
 
-    
+
+
+
+
+
+
+
+
 
 
     print(queryset)
 
-   
+
 
 
 
